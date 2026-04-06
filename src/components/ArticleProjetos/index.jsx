46692928772ba3f7b20aad6b6067acde0ca7b23e
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-04-06 09:41:58 -0300

    pequeno erro

--- src/components/ArticleProjetos/index.jsx
+++ src/components/ArticleProjetos/index.jsx
@@ -92,7 +92,6 @@ export default function ArticleProjetos() {
   return (
     <article id="projetos">
       <h2>
-        {" "}
         <i className="fa-solid fa-trowel-bricks"></i> Projetos Criados
       </h2>
 
